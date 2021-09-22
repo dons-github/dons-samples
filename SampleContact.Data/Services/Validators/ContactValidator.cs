e706c3782566b48d1f6787eceeae1032581d64f3
Donald Manis <don2013@outlook.com>
2021-09-22 08:52:57 -0400

    Added more documentation

--- SampleContact.Data/Services/Validators/ContactValidator.cs
+++ SampleContact.Data/Services/Validators/ContactValidator.cs
@@ -3,10 +3,20 @@ using SampleContact.Data.Models;
 
 namespace SampleContact.Data.Services.Validators
 {
+    /// <summary>
+    /// This class contains the validation for a <see cref="Contact"/>
+    /// </summary>
     public class ContactValidator : AbstractValidator<Contact>
     {
+        /// <summary>
+        /// The validation rules are defined in this constructor
+        /// </summary>
         public ContactValidator()
         {
+            // Normally, I would contact the business / business anaylist for specific rules
+            // In this case, I created a few just for demonstration purposes.
+            // TODO - Document these rules
+
             RuleFor(c => c.Email)
                 .NotNull()
                 .WithMessage("Email is required!");
